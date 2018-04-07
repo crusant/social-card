9409268e203d863d74627443fa117de7c68a1293
Luis Santos <lesc135c@gmail.com>
2018-04-07 09:00:32 -0500

    Se añadió al botón de compartir la clase y atributo del componente tooltip

--- src/components/Footer/Share/index.js
+++ src/components/Footer/Share/index.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './index.css'
+import '../../UI/Tooltip/index.css'
 import IconButton from '../../UI/IconButton'
 
 const share = ({ handleShare }) => (
-  <IconButton className="Share" icon="share" handleClick={handleShare} />
+  <IconButton className="Share Tooltip" icon="share" data-tooltip="Compartir" handleClick={handleShare} />
 )
 
 export default share
